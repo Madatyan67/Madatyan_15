84871387bf12459ed1eb8cae6fe553f48283bc59
Madatyan67 <96314593+Madatyan67@users.noreply.github.com>
2021-12-18 00:00:04 +0300

    Madatyan_15

---
+++ Madatyan_15/Madatyan_15/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Keshishyan15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int t = Convert.ToInt32(textBox1.Text);
+            if (radioButton1.Checked)
+            { int oplata = t * 200; textBox2.Text = Convert.ToString(oplata); }
+            else if (radioButton2.Checked)
+            { int oplata = t * 300; textBox2.Text = Convert.ToString(oplata); }
+            else if (radioButton3.Checked)
+            { int oplata = t * 400; textBox2.Text = Convert.ToString(oplata); }
+        }
+
+    }
+}
