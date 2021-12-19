a0b56d4813fbc286f1ac8812845a556286203385
Madatyan67 <96314593+Madatyan67@users.noreply.github.com>
2021-12-19 18:55:35 +0300

    Update AssemblyInfo.cs

--- Madatyan_15/Madatyan_15/Properties/AssemblyInfo.cs
+++ Madatyan_15/Madatyan_15/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("Keshishyan15")]
+[assembly: AssemblyTitle("Madatyan15")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Keshishyan15")]
+[assembly: AssemblyProduct("Madatyan15")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
